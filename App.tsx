b4ed48720fa7cec4471cc400c7b0ba2ecd207bfa
Dario Garay Rojo <dariogarayrojo@gmail.com>
2026-08-26 08:33:26 -0600

    Alert in request JSON, Pressable in Request, paddingTop on App

--- App.tsx
+++ App.tsx
@@ -16,6 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop:100,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
--- components/Request.tsx
+++ components/Request.tsx
@@ -1,45 +1,48 @@
 import { useState, useEffect } from 'react';
-import { View, Text, ActivityIndicator, FlatList, Pressable } from 'react-native';
+import { View, Text, ActivityIndicator, FlatList, Pressable, Alert } from 'react-native';
 
 export default function RequestFunction(props: any) {
-    
-    const[data, setData] = useState([]);
+        
+        const[data, setData] = useState([]);
 
-    async function request() {
-        // do the actual request and get a response
-        var response = await fetch(props.url);
+        async function request() {
+            // do the actual request and get a response
+            var response = await fetch(props.url);
 
-        // parse response into a json
-        var json = await response.json();
+            // parse response into a json
+            var json = await response.json();
 
-        console.log(json);
-        setData(json);
-    }
-
-    // how to fix the loop? hooks!
-    useEffect(() => {
-        request();
-    }, []);
-
-    return (
-    <View>
-        {
-            data.length > 0 ?
-                <FlatList 
-                    data={data}
-                    renderItem={({item}) => {
-                        return(
-                            <View>
-                                <Text>{item['marca']}</Text>
-                                <Text>{item['modelo']}</Text>
-                                <Text>{item['anio']}</Text>
-                            </View>
-                        );
-                    }}
-                />
-            :
-                <ActivityIndicator size="large" />
+            console.log(json);
+            setData(json.instrumentos);
         }
-    </View>
-    );
+
+        // how to fix the loop? hooks!
+        useEffect(() => {
+            request();
+        }, []);
+
+        return (
+        <View>
+            {
+                data.length > 0 ?
+                    <FlatList 
+                        data={data}
+                        renderItem={({item}) => {
+                            return(
+                                <View>
+                                    <Pressable onPress={() => {Alert.alert(
+                                        item['nombre'],
+                                        `Familia: ${item['familia']}\nOrigen: ${item['origen']}\nPrecio: $${item['precioAproximado']}`
+                                    )}}>
+                                    <Text>{item['nombre']}</Text>
+                                    </Pressable>
+                                </View>
+                            );
+                        }}
+                    />
+                :
+                    <ActivityIndicator size="large" />
+            }
+        </View>
+        );
 }
